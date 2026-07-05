b27fcfc94b2b761f31740282df3868be0fc76abf
bayardcoin <pitcoinchaser@gmail.com>
2026-07-05 00:22:50 -0500

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window.CONFIG = {
+  // Replace this with your Supabase project URL
+  SUPABASE_URL: "https://inqvouwyuejgbadygxjm.supabase.co",
+
+  // Replace this with your Supabase Publishable Key
+  SUPABASE_KEY: "PASTE_YOUR_PUBLISHABLE_KEY_HERE"
+};
